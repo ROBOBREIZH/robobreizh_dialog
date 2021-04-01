90d9eea08446e3c1e65b6af4915734069b8b0002
maelic <teoneau@gmail.com>
2021-04-01 14:46:22 +0200

    debug install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
 #Install and train the Question Classification module
-pip3 install numpy -y
-pip3 install gensim -y 
-pip3 install sklearn -y
-cd Question_Classification/data
-wget --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1saFGKezSFgH-5YjsQX_yiWes41xqbHrf' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p' >> tmp.txt
-code=`cat tmp.txt`
-wget --load-cookies cookies.txt 'https://docs.google.com/uc?export=download&confirm='$code'&id=1saFGKezSFgH-5YjsQX_yiWes41xqbHrf' -O glove.txt
-rm tmp.txt
-rm cookies.txt
-cd .. 
-python3.7 train.py
+sudo python3.7 -m pip install numpy 
+sudo python3.7 -m pip install gensim 
+sudo python3.7 -m pip install sklearn 
 
 #Install Genderize library for genderization of Named Entity 
-pip3 install Genderize -y
+sudo python3.7 -m pip install Genderize 
 
 #Install Transformers to perform context-based Question-Answering 
-pip3 install transformers -y
+sudo python3.7 -m pip install transformers 
 
 #Install spacy for tokenization
-pip3 install spacy -y
+sudo python3.7 -m pip install spacy 
 python3.7 -m spacy download en_core_web_sm
 
 #Install wikipedia API
-pip3 install wikipedia -y
+sudo python3.7 -m pip install wikipedia
 
 #Install NLTK (used only on previous versions and/or futur versions)
-pip3 install nltk -y
+sudo python3.7 -m pip install nltk 
 python3.7 -c "import nltk; nltk.downloader.download('vader_lexicon');"
 
 # CASA Dependencies
@@ -36,5 +28,5 @@ sudo python3.7 -m pip install -r requirements.txt
 
 #PyAudio
 sudo apt install portaudio19-dev -y
-sudo python2.7 -m pip install PyAudio -y
-sudo python2.7 -m pip install SpeechRecognition -y
+sudo python2.7 -m pip install PyAudio 
+sudo python2.7 -m pip install SpeechRecognition 
